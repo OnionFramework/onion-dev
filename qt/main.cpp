bd0f5fc5b1de9793c4707011ec7342de61bf698f
OnionFramework <onion.mhframework@gmail.com>
2023-02-14 01:04:02 -0300

    General testing

--- qt/main.cpp
+++ qt/main.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
+#include "onion/class_info.hpp"
 
 using namespace std;
 
+class X : public onion::ClassInfo
+{
+public:
+
+    X(const ClassInfo& ci): ClassInfo(ci){}
+    virtual ~X() { std::cout << "X dtor\n"; }
+    virtual void operator()(void) = 0;
+};
+
+class Y : public X{
+public:
+    Y():X( ClassInfoSet()
+           .name("X")
+           .version_tag("v0.2.0")
+           .purpose("Teste")){}
+
+    virtual ~Y() { std::cout << "Y dtor\n"; }
+    void operator()(void){
+       std::cout << "Y() called\n";
+    }
+};
+
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    X* x = new Y;
+
+    std::cout<< x;
+
+    delete x;
+
+    /*
+    // create each plugabble object
+
+    // decorate it
+    // connect it to the algorithm
+    // run
+
+    onion::algorithm::LocalSearch localSearch = local_search::Create()
+        .CreateOperator( create );
+        .NeighborhoodOperator( neighborhood );
+        .ObjectiveOperator( objective );
+        .SelectOperator( select );
+        .InnerLoopController( inner_loop )
+        .OuterLoopController( outer_loop );
+
+    localSearch.run();
+
+    localSearch.setNeighborOperator(...)
+    localSearch.setCreateOperator(...)
+    localsearch.set
+
+
+
+
+
+    */
 }
--- qt/qt.pro
+++ qt/qt.pro
@@ -3,5 +3,27 @@ CONFIG += console c++17
 CONFIG -= app_bundle
 CONFIG -= qt
 
-SOURCES += \
-        main.cpp
+INCLUDEPATH += "../../onion/"
+SOURCES += main.cpp \
+    ../../onion/onion/class_info.cpp
+
+DISTFILES += \
+    ../../onion/onion.png \
+    ../../onion/LICENSE.md \
+    ../../onion/README.md
+
+HEADERS += \
+    ../../onion/cops/functions/rv_functions.hpp \
+    ../../onion/cops/mkp/mkp.hpp \
+    ../../onion/cops/tsp/array/create.hpp \
+    ../../onion/cops/tsp/array/tsp.hpp \
+    ../../onion/cops/tsp/bitmatrix/tsp.hpp \
+    ../../onion/onion/class_info.hpp \
+    ../../onion/onion/create.hpp \
+    ../../onion/onion/local_search.hpp \
+    ../../onion/onion/non_copyable.hpp \
+    ../../onion/onion.hpp \
+    ../../onion/cops/cop_info.hpp
+
+
+
