e0a47b509f81d92ad2a696fcbf5aef8911e27805
OnionFramework <onion.mhframework@gmail.com>
2023-02-15 17:45:28 -0300

    Initial development

--- qt/main.cpp
+++ qt/main.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
-#include "cops/tsp/array/create.hpp"
-#include "onion/component.hpp"
+//#include "onion/component.hpp"
+//#include "cops/tsp/array/create_random.hpp"
+#include "onion/random.hpp"
 
 using namespace std;
 
-onion::cops::tsp::array::CreateRandom<10> createRnd;
-onion::cops::tsp::array::CreateGreedy<10> createGreedy;
+//onion::cops::tsp::array::CreateRandom<10> createRnd;
+//onion::cops::tsp::array::CreateGreedy<10> createGreedy;
 
 
 int main()
 {
 
-    std::cout<< createRnd << endl;
-    std::cout<< createGreedy << endl;
+//    std::cout<< createRnd << endl;
+//    std::cout<< createGreedy << endl;
 
+    onion::random<> r;
+    std::cout<< r.uniform_real_01() << std::endl << r.uniform_int_between(10,20);
 
     /*
     // create each plugabble object
@@ -42,3 +45,42 @@ int main()
 
     */
 }
+
+//#include <iostream>
+//#include <iomanip>
+//#include <string>
+//#include <map>
+//#include <random>
+//#include <cmath>
+
+//int main()
+//{
+//    // Seed with a real random value, if available
+//    std::random_device r;
+
+//    // Choose a random mean between 1 and 6
+//    std::default_random_engine e1(r());
+//    std::uniform_int_distribution<int> uniform_dist(0,20);
+//    int mean = uniform_dist(e1);
+//    std::cout << "Randomly-chosen mean: " << mean << '\n';
+
+//    // Generate a normal distribution around that mean
+//    std::seed_seq seed2{r(), r(), r(), r(), r(), r(), r(), r()};
+//    std::mt19937 e2(seed2);
+//    std::normal_distribution<> normal_dist(mean, 10);
+
+//    std::map<int, int> hist;
+//    for (int n = 0; n < 40000; ++n) {
+//        //auto v = normal_dist(e2);
+////        auto v = e1();
+////        auto range = 20-0+1;
+////        v =  v % range +0;
+//        auto v = uniform_dist(e2);
+//        hist[std::round(v)]++;
+//    }
+//    std::cout << "Normal distribution around " << mean << ":\n";
+//    for (auto p : hist) {
+//        std::cout << std::fixed << std::setprecision(1) << std::setw(2)
+//                  << p.first << ' ' << std::string(p.second/50, '*') << '\n';
+//    }
+//}
--- qt/qt.pro
+++ qt/qt.pro
@@ -14,8 +14,28 @@ DISTFILES += \
 HEADERS += \
     ../../onion/cops/functions/rv_functions.hpp \
     ../../onion/cops/mkp/mkp.hpp \
-    ../../onion/cops/tsp/array/create.hpp \
-    ../../onion/cops/tsp/array/create.hpp \
+    ../../onion/cops/tsp/array/array.hpp \
+    ../../onion/cops/tsp/array/create_greedy.hpp \
+    ../../onion/cops/tsp/array/create_random.hpp \
+    ../../onion/cops/tsp/array/create_random.hpp \
+    ../../onion/cops/tsp/array/tsp.hpp \
+    ../../onion/cops/tsp/bitmatrix/tsp.hpp \
+    ../../onion/onion/create.hpp \
+    ../../onion/onion/local_search.hpp \
+    ../../onion/onion/non_copyable.hpp \
+    ../../onion/onion.hpp \
+    ../../onion/onion/random.hpp \
+    ../../onion/onionfw.hpp \
+    ../../onion/onion/component.hpp
+
+
+
+
+
+HEADERS += \
+    ../../onion/cops/functions/rv_functions.hpp \
+    ../../onion/cops/mkp/mkp.hpp \
+    ../../onion/cops/tsp/array/create_random.hpp \
     ../../onion/cops/tsp/array/tsp.hpp \
     ../../onion/cops/tsp/bitmatrix/tsp.hpp \
     ../../onion/onion/create.hpp \
