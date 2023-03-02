4887daf3cd970d676f3058e9178a5abaa0c067be
OnionFramework <onion.mhframework@gmail.com>
2023-03-02 08:46:54 -0300

    developing the Algorithm concept

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "test_rng.hpp"
+#include <cstdlib>
 
 /*
 
@@ -50,14 +51,91 @@ localSearch()( SetComponents<LocalSearch>()
  ENTANGLED
 
 */
+template <typename T>
+struct Maybe{
+
+    virtual ~Maybe() = default;
+
+    Maybe():is_nothing(true){}
+    Maybe(const T& v):_v(v),is_nothing(false){}
+
+    bool operator ==(const Maybe& rhs) const{
+        return (this->is_nothing == rhs.is_nothing);
+    }
+
+    bool operator !=(const Maybe& rhs) const{
+        return (*this == rhs);
+    }
+
+    virtual T operator()() const = 0;
+    virtual operator const char*() const = 0;
+
+protected:
+    T _v;
+private:
+    bool is_nothing;
+};
+
+template<typename T>
+struct Just : public Maybe<T>{
+    Just(const T& v): Maybe<T>(v){}
+    virtual T operator()() const { return Maybe<T>::_v;}
+    operator const char*() const{
+        static char buf[64];
+        std::sprintf(buf,"index [%d]",Maybe<T>::_v);
+        return buf;
+    }
+};
+
+template<typename T>
+struct Nothing : public Maybe<T> {
+    Nothing() = default;
+    virtual T operator()() const { throw std::runtime_error("Cant get a value from nothing.");}
+    operator const char*() const{
+        return "nothing";
+    }
+};
 
 #include "onion/ComparissonOperator.hpp"
 
+Maybe<int>& select(int p){
+    static Nothing<int> nothing;
+    static Just<int> just(0);
+
+    if (p){
+        just = Just<int>(p);
+        return just;
+    }
+    else{
+        return nothing;
+    }
+}
+
+class A{
+   bool operator==(const A&){ return true;}
+};
+
+namespace CHECK
+{
+  struct No {};
+  template<typename T, typename Arg> No operator== (const T&, const Arg&);
+
+  template<typename T, typename Arg = T>
+  struct EqualExists
+  {
+    enum { value = !std::is_same<decltype(std::declval<T>() < std::declval<Arg>()), No>::value };
+  };
+}
+
 int main()
 {
-    ComparissonOperator<int> c = LessOrEqual<int>;
-
-    if ( c(10,9) ) std::cout<<"10 é menor que 20!\n";
-    else std::cout<<"10 é MAIOR que 20!?\n";
+    if ( CHECK::EqualExists<A>::value ){
+        std::cout<< "EqualExists\n";
+    }
+    else{
+       std::cout<< "NO equal op here\n";
+    }
 
 }
+
+
