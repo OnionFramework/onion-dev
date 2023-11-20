7598084caa4093288eb70e7c714d2b38ba8d98bb
André Ladeira @ KUbuntu-DELL <onion.mhframework@gmail.com>
2023-11-19 21:38:25 -0300

    added build number feature to the doxygen documentation page

---
+++ doxygen/makefile_ubuntu.sh
@@ -0,0 +1,7 @@
+OLD=$(< buildnumber.txt)
+NEW=$(($OLD + 1))
+echo $NEW > buildnumber.txt
+sed -ri "s/Version&message=([0-9]+.[0-9]+.[0-9]+) build [0-9]+/Version\&message=\1 build $NEW/g" mainpage.md
+doxygen Doxyfile
+echo 
+echo "=====  BUILD $NEW ====="
